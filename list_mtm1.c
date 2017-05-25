941e0fa202271ff28a9db5e495f62db4a05fe061
sapirga <gabizons@campus.technion.ac.il>
2017-05-25 14:07:11 +0300

    Initial commit

---
+++ list_mtm1.c
@@ -0,0 +1,141 @@
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+#include <malloc.h>
+#include "list_mtm1.h"
+#include <assert.h>
+
+typedef struct Snode{
+    ListElement element;
+    struct Snode* next;
+}Node;
+
+struct list_t{
+    int list_size;
+    Node* header;
+    Node* iterator;
+    CopyListElement copyFunc;
+    FreeListElement freeFunc;
+};
+
+static Node* createNode(CopyListElement copyElement, ListElement element);
+
+List listCreate(CopyListElement copyElement, FreeListElement freeElement){
+    if (copyElement == NULL || freeElement == NULL) return NULL;
+    List list = malloc(sizeof(*list));
+    if (list == NULL) return NULL;
+    list->header = createNode(copyElement,NULL);
+    if (list->header == NULL) {
+        free(list);
+        return NULL;
+    }
+    list->iterator = NULL;
+    list->list_size = 0;
+    list->copyFunc = copyElement;
+    list->freeFunc = freeElement;
+    return  list;
+}
+
+static Node* createNode(CopyListElement copyElement, ListElement element){
+    assert(copyElement != NULL);
+    Node* node = malloc(sizeof(Node));
+    if (node==NULL) return NULL;
+    if (element == NULL){
+        node->element = NULL;
+    } else {
+        node->element = copyElement(element);
+    }
+    node->next = NULL;
+    return node;
+}
+
+static void freeNode(Node* node, FreeListElement freeElement){
+    assert(node!=NULL && freeElement!=NULL);
+    if (node->element != NULL) {
+        freeElement(node->element);
+    }
+    free(node);
+    return;
+}
+
+void listDestroy(List list){
+    if (list==NULL) return;
+    for (int i=0; i < (list->list_size)+1; i++){
+        if (list->header->next == NULL) {
+            freeNode(list->header, list->freeFunc);
+            break;
+        }
+        list->iterator = list->header->next;
+        list->header->next = list->header->next->next;
+        freeNode(list->iterator,list->freeFunc);
+    }
+    list->list_size=0;
+    list->iterator=NULL;
+    list->header=NULL;
+    list->freeFunc=NULL;
+    list->copyFunc=NULL;
+    free(list);
+    return;
+}
+
+ListResult listInsertFirst(List list, ListElement element){
+    if (list==NULL) return LIST_NULL_ARGUMENT;
+    Node* node = createNode(list->copyFunc,element);
+    if (node == NULL) return LIST_OUT_OF_MEMORY;
+    node->next = list->header->next;
+    list->header->next = node;
+    list->list_size++;
+    return LIST_SUCCESS;
+}
+
+ListResult listInsertLast(List list, ListElement element){
+    if (list==NULL) return LIST_NULL_ARGUMENT;
+    Node* node = createNode(list->copyFunc,element);
+    if (node == NULL) return LIST_OUT_OF_MEMORY;
+    Node* tmp = list->header;
+    for (int i=0 ; i < list->list_size ; i++){
+        tmp = tmp->next;
+    }
+    tmp->next = node;
+    list->list_size++;
+    return LIST_SUCCESS;
+}
+
+List listCopy(List list){
+    if (list == NULL) return NULL;
+    List new_list = listCreate(list->copyFunc,list->freeFunc);
+    if (new_list == NULL) return NULL;
+    Node* tmp = list->header->next;
+    for (int i=0 ; i < list->list_size; i++){
+        char* word = tmp->element;
+        ListResult check = listInsertLast(new_list,tmp->element);
+        if (check != LIST_SUCCESS) {
+            listDestroy(new_list);
+            return NULL;
+        }
+        tmp = tmp->next;
+    }
+    return  new_list;
+}
+
+int listGetSize(List list){
+ if (list == NULL) return  -1;
+    return list->list_size;
+}
+
+List listFilter(List list, FilterListElement filterElement, ListFilterKey key){
+    if (list == NULL || filterElement == NULL) return NULL;
+    List new_list = listCreate(list->copyFunc,list->freeFunc);
+    if (new_list == NULL) return NULL;
+    Node* tmp = list->header->next;
+    for (int i=0 ; i < list->list_size; i++){
+        if (filterElement(tmp->element,key) == 0) continue;
+        ListResult check = listInsertLast(new_list,tmp->element);
+        if (check != LIST_SUCCESS) {
+            listDestroy(new_list);
+            return NULL;
+        }
+        tmp = tmp->next;
+    }
+    return  new_list;
+}
---
+++ listmtm_test.c
@@ -0,0 +1,90 @@
+#include <stdbool.h>
+#include <malloc.h>
+#include "list_mtm1.h"
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <stdlib.h>
+
+#define ASSERT(test_number, test_condition)  \
+if (!(test_condition)) {printf("\nTEST %s FAILED", test_number); } \
+else printf("\nTEST %s PASSED", test_number);
+
+
+static ListElement copyString(ListElement str){
+    assert(str);
+    char* copy = malloc(strlen(str)+1);
+    return copy != NULL ? strcpy(copy, str) : NULL;
+}
+
+static void freeString(ListElement str){
+    free(str);
+}
+
+bool isShorterThan(ListElement str, ListFilterKey length) {
+    return strlen(str) < *(int*) length;
+}
+
+bool isLongerThan(ListElement string, ListFilterKey key) {
+    return strlen(string) > *(int*)key;
+}
+
+int main(int argc, char **argv){
+    //Check listCreate Func
+    List list1 = listCreate(NULL, freeString);
+    ASSERT("1.1", list1 == NULL);
+    list1 = listCreate(copyString, NULL);
+    ASSERT("1.2", list1 == NULL);
+    list1 = listCreate(copyString, freeString);
+    ASSERT("1.3", list1 != NULL);
+    //Check listInsertFirst and listInsertLast Func
+    char* father = "father";
+    char* mother = "mother";
+    ListResult result1 = listInsertFirst(NULL,father);
+    ASSERT("2.1", result1 == LIST_NULL_ARGUMENT);
+    result1 = listInsertFirst(list1,father);
+    ASSERT("2.2", result1 == LIST_SUCCESS);
+    result1 = listInsertFirst(list1,mother);
+    ASSERT("2.3", result1 == LIST_SUCCESS);
+    char* brother = "brother";
+    char* sister = "sister";
+    result1 = listInsertLast(NULL,brother);
+    ASSERT("2.4", result1 == LIST_NULL_ARGUMENT);
+    result1 = listInsertLast(list1,brother);
+    ASSERT("2.5", result1 == LIST_SUCCESS);
+    result1 = listInsertLast(list1,sister);
+    ASSERT("2.6", result1 == LIST_SUCCESS);
+    //Check listGetSize Func
+    int result2 = listGetSize(NULL);
+    ASSERT("3.1", result2 == -1);
+    result2 = listGetSize(list1);
+    ASSERT("3.2", result2 == 4);
+    //Check listCopy Func
+    List list2 = listCopy(NULL);
+    ASSERT("4.1", list2 == NULL);
+    list2 = listCopy(list1);
+    ASSERT("4.2", list2 != NULL && (listGetSize(list1) == listGetSize(list2)));
+    //Check listFilter Func
+    int key = 7;
+    List list3 = listFilter(NULL, isShorterThan,&key);
+    ASSERT("5.1", list3 == NULL);
+    list3 = listFilter(list2, NULL, &key);
+    ASSERT("5.2", list3 == NULL);
+    list3 = listFilter(list2, isShorterThan,&key);
+    ASSERT("5.3", list3 != NULL && (listGetSize(list3) == 3));
+    key = -1;
+    List list4 = listFilter(list2, isShorterThan,&key);
+    ASSERT("5.4", list4 == NULL);
+    key = 6;
+    list4 = listFilter(list2, isLongerThan,&key);
+    ASSERT("5.5", list4 != NULL && (listGetSize(list4) == 1));
+
+    listDestroy(list1);
+    listDestroy(list2);
+    listDestroy(list3);
+    listDestroy(list4);
+    return 0;
+}
+
+
+
